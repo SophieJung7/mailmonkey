acacfec890cd4578912d8ca838918f8b59869724
Sophie <sophie@Chris.local>
2019-10-15 17:26:28 +0800

    GoogleOAuth Setup Done

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
-const app = express();
 const authRoutes = require('./routes/authRoutes');
 require('./models/User');
 require('./services/passport');
-authRoutes(app);
 
 mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+// **** Enabling Cookies **** // 
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    // Cookie lasts for 30 days
+    keys: [keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// **** Routes **** // 
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
