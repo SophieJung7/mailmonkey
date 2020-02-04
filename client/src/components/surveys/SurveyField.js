393786c36df610d70b438c26ed2797a9efe8e73a
Sophie Jung <sophiecheong7@gmail.com>
2020-02-04 11:29:43 +0800

    Reset the survey form for practicing reduxForm

--- client/src/components/surveys/SurveyField.js
+++ client/src/components/surveys/SurveyField.js
@@ -1,14 +1,1 @@
-// SurveyField contains logic to render a single label and text input.
-import React from 'react'
 
-const SurveyField = ({ input, label, meta: { error, touched } }) => {
-    return (
-        <div className="form-group">
-            <label>{ label }</label>
-            <input { ...input } className="form-control rounded-0" />
-            <small className="form-text text-muted">{ touched && error }</small>
-        </div>
-    )
-}
-
-export default SurveyField;
--- client/src/components/surveys/formFields.js
+++
@@ -1,6 +0,0 @@
-export default [
-    { label: 'Survey Title', name: 'title', noValueError: 'Provide a survey title.' },
-    { label: 'Subject Line', name: 'subject', noValueError: 'Provide a survey subject.' },
-    { label: 'Email Body', name: 'body', noValueError: 'Provide a survey body.' },
-    { label: 'Recipient List', name: 'emails', noValueError: 'Provide emails of recipients.' }
-];
