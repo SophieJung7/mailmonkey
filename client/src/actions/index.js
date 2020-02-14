314b769b74ce7f3fd640f7e720fb2801bd04ce9e
Sophie Jung <sophiecheong7@gmail.com>
2020-02-14 12:40:29 +0800

    Made action creator for survey post request

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import history from "../../src/history";
-import { FETCH_USER, SUBMIT_SURVEY } from "./types";
+import { FETCH_USER } from "./types";
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get("/api/current_user");
@@ -21,8 +21,8 @@ export const handleStripeToken = token => async dispatch => {
 export const submitSurvey = values => async dispatch => {
     const res = await axios.post("/api/surveys", values);
     dispatch({
-        type: SUBMIT_SURVEY,
+        type: FETCH_USER,
         payload: res.data
     });
-    history.push("/api/surveys/thanks");
+    history.push("/surveys");
 };
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,43 +2,39 @@ const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
-const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+        const { title, subject, body, recipients } = req.body;
 
-  app.get('/api/surveys/thanks', (req, res) => {
-    res.send('Thanks for voting!');
-  });
-  
-  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-    const { title, subject, body, recipients } = req.body;
+        const survey = new Survey({
+            title,
+            subject,
+            body,
+            recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+            _user: req.user.id,
+            dateSent: Date.now()
+        });
 
-    const survey = new Survey({
-      title,
-      subject,
-      body,
-      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
-      _user: req.user.id,
-      dateSent: Date.now()
+        // Great place to send an email!
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+
+        // Error Handling
+        try {
+            await mailer.send();
+            await survey.save();
+            // Deduct the credit & Save the credit information and send the saved info.
+            req.user.credits -= 1;
+            const user = await req.user.save();
+            res.send(user);
+        } catch (err) {
+            res.status(422).send(err);
+        }
+
     });
-
-    // Great place to send an email!
-    const mailer = new Mailer(survey, surveyTemplate(survey));
-
-    // Error Handling
-    try {
-      await mailer.send();
-      await survey.save();
-      // Deduct the credit & Save the credit information and send the saved info.
-      req.user.credits -= 1;
-      const user = await req.user.save();
-      res.send(user);
-    } catch (err) {
-      res.status(422).send(err);
-    }
-  
-  });
 };
+
 
