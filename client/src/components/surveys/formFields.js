6aeb7d6a77c5d00171fdb4379cb10832a7b7cf42
Sophie Jung <sophiecheong7@gmail.com>
2020-02-14 10:55:34 +0800

    Changed formField label from Email Body to Survey Body

--- client/src/components/surveys/formFields.js
+++ client/src/components/surveys/formFields.js
@@ -1,6 +1,6 @@
 export default [
     { label: 'Survey Title', name: 'title', noValueError: 'Provide a survey title.' },
     { label: 'Subject Line', name: 'subject', noValueError: 'Provide a survey subject.' },
-    { label: 'Email Body', name: 'body', noValueError: 'Provide a survey body.' },
+    { label: 'Survey Body', name: 'body', noValueError: 'Provide a survey body.' },
     { label: 'Recipient List', name: 'emails', noValueError: 'Provide emails of recipients.' }
 ];
